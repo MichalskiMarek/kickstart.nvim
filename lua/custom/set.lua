63008911d5d093ca0672ebcce641f0e4f156b0f9
marekmichalski <marek.michalski@toolkit3d.com>
2024-03-06 13:28:27 +0100

    pre merge

--- lua/custom/set.lua
+++ lua/custom/set.lua
@@ -1,3 +1,8 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.g.copilot_assume_mapped = true
+
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
@@ -12,7 +17,7 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
@@ -21,9 +26,9 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+vim.opt.signcolumn = 'yes'
+vim.opt.isfname:append '@-@'
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = '80'
