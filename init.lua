fae09bf7588d143c4adeaa3103bff97a8725580a
marekmichalski <marek.michalski@toolkit3d.com>
2024-03-06 13:53:12 +0100

    add ps remap

--- init.lua
+++ init.lua
@@ -817,6 +817,10 @@ require('lazy').setup {
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   { import = 'custom.plugins' },
+  -- Remap for grep string
+  vim.keymap.set('n', '<leader>ps', function()
+    require('telescope.builtin').grep_string { search = vim.fn.input 'Grep > ' }
+  end),
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
