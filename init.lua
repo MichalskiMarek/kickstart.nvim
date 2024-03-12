731c881d10156b51a778e92ecbde08f76bf1e991
marekmichalski <marek.michalski@toolkit3d.com>
2024-03-12 17:08:04 +0100

    add gopls build tags setup

--- init.lua
+++ init.lua
@@ -535,7 +535,15 @@ require('lazy').setup {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {
+          settings = {
+            gopls = {
+              usePlaceholders = true,
+              buildFlags = { '-tags=test_unit' },
+              gofumpt = true,
+            },
+          },
+        },
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -544,7 +552,12 @@ require('lazy').setup {
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
+        tsserver = {},
+        cssls = {},
+        eslint = {},
+        graphql = {},
+        html = {},
+        tailwindcss = {},
         --
 
         lua_ls = {
