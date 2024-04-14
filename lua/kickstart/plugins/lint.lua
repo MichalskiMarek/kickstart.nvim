88775fce86ca9ecc63483a82c1c41df54b2ef380
maro <michalskimarek89@gmail.com>
2024-04-14 09:30:43 +0200

    add linting plugins

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,6 +7,9 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        json = { 'jsonlint' },
+        text = { 'vale' },
+        dockerfile = { 'hadolint' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
